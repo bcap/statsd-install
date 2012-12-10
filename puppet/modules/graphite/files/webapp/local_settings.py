562423cf5c2cc3d944c4ba3f506bd87d55d3e2a7
Bruno Polaco <bcpenteado@ig.com>
2012-12-10 18:53:26 -0200

    Simplifying configuration back to the previous DATABASES or else local_settings would have to be much like settings.py

--- puppet/modules/graphite/files/webapp/local_settings.py
+++ puppet/modules/graphite/files/webapp/local_settings.py
@@ -42,10 +42,14 @@ TIME_ZONE = 'Europe/Berlin'
 #DATABASE_PORT = '3306'
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(STORAGE_DIR, 'graphite.db'),
-    },
+   'default': {
+        'ENGINE': 'django.db.backends.sqlite3',       # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/opt/graphite/storage/graphite.db',  # Or path to database file if using sqlite3.
+        'USER': '',                                   # Not used with sqlite3.
+        'PASSWORD': '',                               # Not used with sqlite3.
+        'HOST': '',                                   # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                                   # Set to empty string for default. Not used with sqlite3.
+    }
 }
 
 TEMPLATE_LOADERS = (
